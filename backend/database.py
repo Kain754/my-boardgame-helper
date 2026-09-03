019e652998ee535bd28c621b2e8560bb7b842291
Kain754 <pustovalov-00@bk.ru>
2026-09-03 17:58:44 +0300

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -8,16 +8,14 @@ engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
-
 class GameRule(Base):
     __tablename__ = "rules"
-
+    
     id = Column(Integer, primary_key=True, index=True)
     game_name = Column(String(200), index=True)
     page_number = Column(Integer)
     content = Column(Text)
-    image = Column(Text, nullable=True)  # Изменено: теперь Text для base64
+    image = Column(Text, nullable=True)
     image_url = Column(String(500), nullable=True)
-
 
 Base.metadata.create_all(bind=engine)
